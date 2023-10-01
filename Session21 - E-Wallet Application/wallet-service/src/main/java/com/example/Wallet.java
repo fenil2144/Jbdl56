17e2f7328d28a5c0fb0b3e6505bc1f693f9f16dc
Preet Sanghavi <shahfenil998@gmail.com>
2023-10-01 19:48:55 +0530

    Session 21

---
+++ Session21 - E-Wallet Application/wallet-service/src/main/java/com/example/Wallet.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Long userId;
+
+    private String phone;
+
+    private Double balance;
+
+    private String identifierValue;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserIdentifier userIdentifier;
+}
