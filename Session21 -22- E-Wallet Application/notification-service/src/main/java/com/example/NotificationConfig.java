f675ceb0b2b03a16a9f013ac3c2883131f4f978b
Preet Sanghavi <shahfenil998@gmail.com>
2023-10-02 20:50:58 +0530

    Session 22

--- Session21 -22- E-Wallet Application/notification-service/src/main/java/com/example/NotificationConfig.java
+++ Session21 -22- E-Wallet Application/notification-service/src/main/java/com/example/NotificationConfig.java
@@ -40,7 +40,7 @@ public class NotificationConfig {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setHost("smtp.gmail.com");
         javaMailSender.setPort(587);
-        javaMailSender.setUsername("ewallet.gfg.33@gmail.com");
+        javaMailSender.setUsername("ewallet.gfg.56@gmail.com");
         javaMailSender.setPassword("eWallet@123");
 
 
